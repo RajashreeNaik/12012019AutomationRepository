1d1ba9d39be80630047407e21fc19e5ccc850d65
DELL <DELL@DELL-PC>
2019-12-01 15:27:02 +0530

    Committing first java class

---
+++ BasicCorejava/src/variablesanddatatypes/Firstjavaclass.java
@@ -0,0 +1,11 @@
+package variablesanddatatypes;
+
+public class Firstjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to world of Automation");
+
+	}
+
+}
