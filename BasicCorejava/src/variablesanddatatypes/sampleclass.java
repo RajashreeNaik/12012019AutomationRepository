0850e63bbea8b8e61f11591d262c4cb1539db933
DELL <DELL@DELL-PC>
2019-12-07 15:39:24 +0530

    first new file

---
+++ BasicCorejava/src/variablesanddatatypes/sampleclass.java
@@ -0,0 +1,10 @@
+package variablesanddatatypes;
+
+public class sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world!");
+	}
+
+}
